617054293159a4c614bdbabb64a89a26455e6bcd
arfan <khan188993@gmail.com>
2022-01-04 00:30:20 +0600

    steps added

--- app.js
+++ app.js
@@ -3,3 +3,11 @@
   https://bitbucket.org/prof3ssorSt3v3/things-to-know/
   ~/.npmrc - file for npm settings
 **/
+
+/* 
+#add g.js file in your root folder 
+#add scripts in your package.json file 
+# cmd will be yarn gitty message 
+
+
+*/
